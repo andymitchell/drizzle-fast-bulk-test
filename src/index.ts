5deae25ee210a189c6540aa192d405b5d62106eb
Andy Mitchell <andymitchell@Andys-MacBook-Pro.local>
2025-04-13 15:28:11 +0100

    Export the new driver type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import type { DdtDialect, DdtDialectDatabaseMap, DdtSqliteDriver } from "@andyrmitchell/drizzle-dialect-types";
+import type { DdtDialect, DdtDialectDatabaseMap } from "@andyrmitchell/drizzle-dialect-types";
 import { createSchemaDefinitionFile } from "./createSchemaDefinitionFile.js";
 import { DrizzleFastBulkTestGenerator } from "./DrizzleFastBulkTestGenerator.js";
-import type {  TestSqlDb } from "./types.js";
+import type {  DdtDialectDriver, TestSqlDb } from "./types.js";
 
 export {
     DrizzleFastBulkTestGenerator,
@@ -15,5 +15,5 @@ export type {
 export type {
     DdtDialectDatabaseMap,
     DdtDialect,
-    DdtSqliteDriver
+    DdtDialectDriver
 }
